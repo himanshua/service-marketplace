1f28077d24b0ba2da2537498607c4718ee985ed7
himanshua <himanshu.inperson@gmail.com>
2025-10-07 23:38:48 +0530

    Chore: remove TS and skip ESLint for build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-  // Add custom options here if you had them in next.config.ts
-  // e.g. images: { domains: ['example.com'] }
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
 };
 
 module.exports = nextConfig;
