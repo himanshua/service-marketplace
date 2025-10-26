1236ca927ca6788d73f59037e1d9feebeb8f9a06
himanshua <himanshu.inperson@gmail.com>
2025-10-26 16:59:08 +0530

    admin3

--- frontend/app/admin/services/page.js
+++ frontend/app/admin/services/page.js
@@ -85,7 +85,7 @@ export default function AdminServices() {
   };
 
   const fetchServicesByUser = async () => {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+    //const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
     const data = await fetch("https://service-marketplace-backend.onrender.com/api/services?provider=" + userId);
     setServices(data);
   };
