e2ddec72f2261117e18234cbae754fd859a879a5
himanshua <himanshu.inperson@gmail.com>
2025-10-27 20:38:21 +0530

    admin4

--- frontend/app/admin/services/page.js
+++ frontend/app/admin/services/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function AdminServices() {
   const [services, setServices] = useState([]);
