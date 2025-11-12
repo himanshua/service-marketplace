947b979941647b0d91c7fedc83405407fdc4a4f4
himanshua <himanshu.inperson@gmail.com>
2025-11-12 19:12:44 +0530

    admin90

--- frontend/app/services/page.js
+++ frontend/app/services/page.js
@@ -129,7 +129,7 @@ export default function ServicesPage() {
                       onClick={(e) => e.stopPropagation()}
                     >
                       <button style={{ width: "100%", marginTop: 12 }}>
-                        Chat | {service.price}/MIN
+                        Chat | {service.price}/READING
                       </button>
                     </Link>
                   )}
