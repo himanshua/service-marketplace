c84bad225680df77e377ff070344dc4d8d0cd985
himanshua <himanshu.inperson@gmail.com>
2025-11-22 21:28:57 +0530

    22Nov6

--- frontend/app/Home.js
+++ frontend/app/Home.js
@@ -297,7 +297,7 @@ export default function Home() {
         <div style={{ display: "flex", borderBottom: "2px solid #bdbdbd" }}>
           <div className="home-image-col" style={{ borderRight: "2px solid #bdbdbd" }}>
             <img
-              src="/images/12-houses.jpg"
+              src="/images/12-house.jpg"
               alt="12 Houses"
               className="home-hero-image"
               style={{
