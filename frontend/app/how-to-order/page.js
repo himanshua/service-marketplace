6e6db39d121c29204cf660d9d9399285c00e8157
himanshua <himanshu.inperson@gmail.com>
2025-11-28 01:13:53 +0530

    27Nov3

--- frontend/app/how-to-order/page.js
+++ frontend/app/how-to-order/page.js
@@ -1,6 +1,63 @@
 "use client";
 import Link from "next/link";
-import HomeRow from "../HomeRow";
+import "./profile/profile.css";
+import "./globals.css";
+
+// Copy HomeRow component here
+function HomeRow({ label, imgSrc, imgAlt, imgStyle = {}, children }) {
+  return (
+    <div style={{
+      display: "flex",
+      borderBottom: "2px solid #bdbdbd",
+      alignItems: "stretch",
+      minHeight: 250
+    }}>
+      <div className="home-image-col">
+        <div style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+        }}>
+          {label && <span style={{ marginBottom: 8, fontWeight: 600 }}>{label}</span>}
+          <img
+            src={imgSrc}
+            alt={imgAlt}
+            className="home-hero-image"
+            style={{
+              borderRadius: "12px",
+              width: "100%",
+              maxWidth: 340,
+              height: 260,
+              maxHeight: 260,
+              objectFit: "contain",
+              background: "#fff",
+              display: "block",
+              margin: "0 auto",
+              ...imgStyle,
+            }}
+          />
+        </div>
+      </div>
+      <div
+        className="home-content-col"
+        style={{
+          flex: 1,
+          padding: "18px 24px",
+          boxSizing: "border-box",
+          background: "#fff",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
 
 export default function HowToOrder() {
   return (
