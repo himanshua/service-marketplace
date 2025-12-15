9230c4ad59bc1082cffa4ca0bb2aac85155fe1ba
himanshua <himanshu.inperson@gmail.com>
2025-12-16 01:45:51 +0530

    16

--- frontend/app/15/Page15Client.jsx
+++ frontend/app/15/Page15Client.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React, { useState } from "react";
 import "../globals.css";
 import "../profile/profile.css";
 import UniversalShareBar from "../components/UniversalShareBar";
